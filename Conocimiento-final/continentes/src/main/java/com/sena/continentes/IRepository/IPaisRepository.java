bb70dd1ed76b845764b10bfd3828ffe513a27ccc
ErickPea <91999653+ErickPea@users.noreply.github.com>
2023-12-12 20:43:04 -0500

    Delete Conocimiento-final directory

--- Conocimiento-final/continentes/src/main/java/com/sena/continentes/IRepository/IPaisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sena.continentes.IRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sena.continentes.Entity.Pais;
-
-public interface IPaisRepository extends JpaRepository<Pais, Long> {
-}
